c9125d18f7ba204bbae08442f4ccef562084d1f1
HIROAKI-KAJI <kaji.tolearnprogramming@gmail.com>
2023-05-09 17:17:48 +0900

    update S006

--- S006/copy_nintendo_app/lib/main.dart
+++ S006/copy_nintendo_app/lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:copy_nintendo_app/home.dart';
 
 void main() {
-  final home = Home();
-  final app = MaterialApp(
+  const home = Home();
+  const app = MaterialApp(
     home: home,
     debugShowCheckedModeBanner: false,//チェック版のマーク削除
     );
-  final scopedApp = ProviderScope(child : app);
+  const scopedApp = ProviderScope(child : app);
   runApp(scopedApp);
 }
