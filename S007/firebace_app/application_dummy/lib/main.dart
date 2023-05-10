0c25969f6b6e7ecb8d95ef83b74db6b575cc293a
HIROAKI-KAJI <kaji.tolearnprogramming@gmail.com>
2023-05-10 16:30:30 +0900

    update S007/firebase_app

--- S007/firebace_app/application_dummy/lib/main.dart
+++ S007/firebace_app/application_dummy/lib/main.dart
@@ -6,14 +6,11 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Center(child: Text('Hello World!')),
       ),
     );
   }
