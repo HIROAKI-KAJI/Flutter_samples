ef50e72360e2e5f0b82419ae18b0a286b2b4417d
HIROAKI-KAJI <kaji.tolearnprogramming@gmail.com>
2023-05-14 21:44:31 +0900

    make S008

---
+++ S008/bapp/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text(
+                "test"
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
